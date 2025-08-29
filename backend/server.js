1aa440704221ef73c40344b0075d690356f73440
priymavani <priy.mavani.cg@gmail.com>
2025-08-30 03:35:42 +0530

    main server file for backend

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { errorHandler } = require('./middleware/errorHandler');
+// const designRoutes = require('./routes/promptRoute');
+const chatRoutes = require('./routes/chatRoute');
+const generationRoutes = require('./routes/generationRoute'); 
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+// app.use('/api/design', designRoutes);
+app.use('/api/chat', chatRoutes);
+app.use('/api/generate', generationRoutes); 
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
